e00c2ac8c1fe367e59fd0c3d94ef29e467e24dae
Avi Bansal <avi.bansal2019@icloud.com>
2023-12-12 20:29:41 -0800

    Add files via upload

---
+++ primitives/point.js
@@ -0,0 +1,19 @@
+class Point  {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  equals(point) {
+      return this.x === point.x && this.y === point.y;
+  }
+
+  draw(ctx, size = 18, color = "black") {
+       const rad = size / 2;
+       ctx.beginPath();
+       ctx.fillStyle = color;
+       ctx.arc(this.x, this.y, rad, 0, 2 * Math.PI);
+       ctx.fill();
+
+   }
+}
